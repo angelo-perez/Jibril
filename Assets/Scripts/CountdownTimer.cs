574aad3b44029385ac6e3b9905304d863eca5702
Dyst <jpmayo1234@gmail.com>
2021-01-26 14:09:41 +0800

    Single Player and Main menu
    > Main menu
    - okay na yung sa main bg
    > Single player
    - nag dagdag ng milliseconds

---
+++ Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CountdownTimer : MonoBehaviour
+{
+    [SerializeField] float timer = 10f;
+    [SerializeField] TextMeshProUGUI timerText;
+    float time = 0f;
+    void Start()
+    {
+        StartCoroutine(CountdownBegins());
+    }
+
+    private IEnumerator CountdownBegins()
+    {
+        time = timer;
+        do
+        {
+            time -= Time.deltaTime;
+            FormatText();
+            yield return null;
+        } while (time > 0);
+    }
+    private void FormatText()
+    {
+        int minutes = (int)(time/60) % 60;
+        float seconds = (time % 60); 
+        string secString = seconds.ToString("F2");
+        timerText.text = "";
+        timerText.text = minutes +":" + secString;
+    }
+}
--- Assets/Scripts/WordChecker.cs
+++ Assets/Scripts/WordChecker.cs
@@ -1,7 +1,10 @@
-﻿using ForSinglePlayer;
+﻿using System.Collections;
+using System.Collections.Generic;
+using ForSinglePlayer;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 
 namespace WordCheck
@@ -13,12 +16,18 @@ namespace WordCheck
         public int score;
         public float currentTime;
         public float startingTime;
+
+        [SerializeField] float timer = 10f;
+        [SerializeField] TextMeshProUGUI timerText;
+        float time = 0f;
+
+
         public GameObject inputField;
         public GameObject outputField;
         public GameObject gameOver;
 
+
         public GameObject scoreObject;
-        public GameObject timeObject;
         public GameObject audio;
         public InputField input; 
 
@@ -33,6 +42,7 @@ namespace WordCheck
             referenceWord = WordManager.GetRandomWord(); // gets random word
             outputField.GetComponent<Text>().text = referenceWord;
             WordManager.RemoveWord(referenceWord);
+            StartCoroutine(CountdownBegins());
             currentTime = startingTime;
             PlayerPrefs.SetInt("score", 0);
             score = 0;
@@ -56,10 +66,7 @@ namespace WordCheck
         {
             if (inputWord == "")
             {
-                currentTime = 0;
-                PlayerPrefs.SetInt("score", score);
-                gameOver.SetActive(true);
-                audio.SetActive(false);
+                GameEnder();
             }
             else if (referenceWord[referenceWord.Length - 1] == inputWord[0] //index: referenceWord.Length - 1
                 && WordManager.wordList.Contains(inputWord))
@@ -73,47 +80,57 @@ namespace WordCheck
             }
             else
             {
-                /*
-                gameOver.GetComponent<Text>().text = "GAME OVER\nWould You like to try again?";
-                
-                
-                yesButton.SetActive(true);
-                noButton.SetActive(true);
-                bgGameOver.SetActive(true);
-                
-                SceneManager.LoadScene("GameOver");
-                */
-                PlayerPrefs.SetInt("score", score);
-                currentTime = 0;
-                gameOver.SetActive(true);
-                audio.SetActive(false);
+                GameEnder();
             }
         }
 
         void Update() //for time
         {
             currentTime -= 1 * Time.deltaTime;
-            timeObject.GetComponent<Text>().text = currentTime.ToString("0");
-
             if (currentTime <= 0)
             {
-                /*
-                currentTime = 0;
-                gameOver.GetComponent<Text>().text = "GAME OVER\nWould You like to try again?";
-                yesButton.SetActive(true);
-                noButton.SetActive(true);
-                bgGameOver.SetActive(true);
-                
-                SceneManager.LoadScene("GameOver");
-                */
-                PlayerPrefs.SetInt("score", score);
-                currentTime = 0;
-                gameOver.SetActive(true);
-                audio.SetActive(false);
+                GameEnder();
             }
 
 
 
+        }
+        void GameEnder()
+        {
+            /*
+            currentTime = 0;
+            gameOver.GetComponent<Text>().text = "GAME OVER\nWould You like to try again?";
+            yesButton.SetActive(true);
+            noButton.SetActive(true);
+            bgGameOver.SetActive(true);
+                
+            SceneManager.LoadScene("GameOver");
+            */
+            PlayerPrefs.SetInt("score", score);
+            currentTime = 0;
+            gameOver.SetActive(true);
+            audio.SetActive(false);
+            timerText.text = "0:00";
+        }
+        
+        IEnumerator CountdownBegins()
+        {
+            time = timer;
+            do
+            {
+                time -= Time.deltaTime;
+                FormatText();
+                yield return null;
+            } while (time > 0);
+
+        }
+        void FormatText()
+        {
+            int minutes = (int)(time/60) % 60;
+            float seconds = (time % 60); 
+            string secString = seconds.ToString("F2");
+
+            timerText.text = minutes +":" + secString;
         }
 
 
